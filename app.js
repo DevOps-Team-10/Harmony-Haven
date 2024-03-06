85d980773b4ca4aed4a3db8ad377eef4ecd4cd2e
Anto, Antony <antony.anto@sap.com>
2024-03-06 23:22:53 +0530

    registration page and its routing

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define the default route to serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/registration', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'registration.html'));
 });
 
 const PORT = process.env.PORT || 3000;
