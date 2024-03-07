ec26bb59c247e86885285a7ee089bcee0e4300a1
Anto, Antony <antony.anto@sap.com>
2024-03-07 23:54:28 +0530

    adding the login page

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
