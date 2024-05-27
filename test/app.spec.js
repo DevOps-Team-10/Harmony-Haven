4760902c1470d3de4413fd80fe3286ac60849e92
Antony Ashwin <2023sl93002@wilp.bits-pilani.ac.in>
2024-05-28 00:35:36 +0530

    JenkinsPipeline check 10

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 describe('Dummy Test', () => {
   it('should pass if the test file is being read', () => {
     // This test will always pass
-    assert.strictEqual(1 + 1, 2);
+    assert.strictEqual(1 + 1, 3);
   });
 
  it('Another dumm test to check pass if the file is being read', () => {
