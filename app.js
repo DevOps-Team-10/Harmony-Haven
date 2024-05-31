923a26b6e20409bd92a5a5e14d2d865a30d60f3a
I528624 <bishal.poudel@sap.com>
2024-05-31 20:00:53 +0530

    socket for chat

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose');
 const {Message} = require('./models/communityModel')
 const cors = require('cors');
 const http = require('http'); 
-
-
+const socketIo = require('socket.io');
 
 const connectToDB = require('./config/db');
 const logger = require('./middleware/logger');
@@ -15,28 +14,20 @@ const routesHandler = require('./routes/handler');
 
 
 
-
 const app = express();
-const socketIo = require('socket.io');
+
 const server = http.createServer(app); 
 const io = socketIo(server);
 
 
-// Connect to Database
-console.log('setting up Database...');
 connectToDB();
 
-//allow cross origin
 app.use(cors());
 app.use(cookieParser())
-
-console.log('Setting up Middleware...');
-// Middleware
 app.use(express.json());
 app.use(logger); // Custom logger middleware
 
-console.log('Setting up Routes...');
-// Routes
+
 app.use('/', routesHandler);
 
 const PORT = process.env.PORT || 5005;
@@ -44,27 +35,28 @@ const PORT = process.env.PORT || 5005;
 
 // Socket.io connection
 io.on('connection', (socket) => {
-  console.log('New client connected');
+  console.log(`New client connected , ID: ${socket.id}`);
+  
 
   socket.on('sendMessage', async (message) => {
     try {
-      const newMessage = new Message({
-        communityId: message.communityId,
-        userId: message.userId,
-        userContent: message.userContent,
-      });
-      await newMessage.save();
-      io.emit('message', newMessage);
+      // const newMessage = new Message({
+      //   communityId: message.communityId,
+      //   userId: message.userId,
+      //   userContent: message.userContent,
+      // });
+      //await newMessage.save();
+      io.emit('message', message);
     } catch (error) {
       console.error('Error saving message:', error);
     }
   });
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
+    console.log(`Client disconnected , id :${socket.id}`);
   });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
